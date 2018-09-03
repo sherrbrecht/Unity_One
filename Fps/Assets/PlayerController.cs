421f9b36ffdedc723943e76a6b0de1fd80f40620
sherrbrecht <35484692+sherrbrecht@users.noreply.github.com>
2018-09-03 16:35:34 +0200

    ok

---
+++ Fps/Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+
+    private PlayerMotor _motor;
+
+    private void Start()
+    {
+        _motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+
+        /* Vélocité du mouvement (...direction & vitesse...) */
+
+        float _xMove = Input.GetAxisRaw("Horizontal");
+        /*
+         -1 = gauche
+         0 = le personnage ne bouge pas
+         1 = droite
+         */
+
+        float _zMove = Input.GetAxisRaw("Vertical");
+        /*
+         -1 = recule
+         0 = le personnage ne bouge pas
+         1 = avance
+         */
+
+        Vector3 _moveHorizontal = transform.right * _xMove;
+        Vector3 _moveVertical = transform.forward * _zMove;
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+
+        _motor.Move(_velocity);
+
+    }
+}
---
+++ Fps/Assets/PlayerMotor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    private Vector3 velocity;
+    private Rigidbody rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+        
+    }
+    private void FixedUpdate()
+    {
+        PerformMovement();
+    }
+
+    private void PerformMovement()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+    
+}
